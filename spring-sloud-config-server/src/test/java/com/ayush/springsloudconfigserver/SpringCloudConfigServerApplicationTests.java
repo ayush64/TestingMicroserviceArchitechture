eea2050812575e47637113cdbd51ecd51260cf46
ayush64 <ayushtyagi64@gmail.com>
2021-04-06 14:11:09 +0530

    Initial commit

---
+++ spring-sloud-config-server/src/test/java/com/ayush/springsloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.springsloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
